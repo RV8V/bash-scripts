767030be06e1fe6c19b8a1128cc58c1034cafea7
Ruslan <rv.volovik88@gmail.com>
2020-07-28 18:22:57 +0300

    changes

--- examples.sh
+++ examples.sh
@@ -5,4 +5,3 @@ find /proc -user username -maxdepth 1 -type d -mtime +7 -exec basename {} \; | x
 find . -type d -print | /usr/bin/xargs -P 4 -I {} /bin/tar czf {}.tar.gz {}
 find . -type f -and -iname '*.deb' | /usr/bin/xargs -n1 -I x dpkg -i x
 dpkg -l linux-* | awk '/^ii/{ print $2 }' | grep -e -v $(uname -r | cut -f1,2 -d"-") | grep -e [0-9]
-
