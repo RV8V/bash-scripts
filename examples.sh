bb3ca2cf18ace61b0d50e599ab26d5dc069702a3
Ruslan <rv.volovik88@gmail.com>
2020-07-28 16:12:25 +0300

    some small changes

--- examples.sh
+++ examples.sh
@@ -3,3 +3,4 @@
 find /tmp -type f -name '*' -mtime +7 -print0 | xargs -p -0 -I % sh -c 'echo %; /bin/rm -f %'
 find /proc -user username -maxdepth 1 -type d -mtime +7 -exec basename {} \; | xargs kill -9
 find . -type d -print | xargs -P 4 -I {} tar czf {}.tar.gz {}
+find . -type f -and -iname '*.deb' | xargs -n1 -I x dpkg `-i x
