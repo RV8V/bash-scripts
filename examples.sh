e56920aa011a6e6c88f89a8a2821d6ae8d5c61fc
Ruslan <rv.volovik88@gmail.com>
2020-07-28 11:24:01 +0300

    some changes

--- examples.sh
+++ examples.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-find /tmp -type f -name '*' -mtime +7 -print0 | xargs -p -0 -I % sh -c 'echo %; rm -f %'
+find /tmp -type f -name '*' -mtime +7 -print0 | xargs -p -0 -I % sh -c 'echo %; /bin/rm -f %'
 find /proc -user username -maxdepth 1 -type d -mtime +7 -exec basename {} \; | xargs kill -9
+find . -type d -print | xargs -P 4 -I {} tar czf {}.tar.gz {}
