7949403736916c001005705ebbf37a5ca8d35bd7
Ruslan <rv.volovik88@gmail.com>
2020-08-02 19:38:05 +0300

    small changes to file

--- bin/examples.sh
+++ bin/examples.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+ 
 find /tmp -type f -name '*' -mtime +7 -print0 | xargs -p -0 -I % sh -c 'echo %; /bin/rm -f %'
 find /proc -user username -maxdepth 1 -type d -mtime +7 -exec basename {} \; | xargs kill -9
 find . -type d -print | /usr/bin/xargs -P 4 -I {} /bin/tar czf {}.tar.gz {}
