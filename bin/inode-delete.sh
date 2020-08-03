da54001bcb62430d6debfe504a54b4a9b4e991b6
Ruslan <rv.volovik88@gmail.com>
2020-08-03 19:22:15 +0300

    delete via inode

---
+++ bin/inode-delete.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+readonly ARGCOUNT=1
+readonly E_WRONGARGS=70
+readonly E_FILE_NOT_EXISTS=71
+readonly E_CHANGED_MIND=72
+
+[ $# -ne $ARGCOUNT ] && echo "Usage: `basename $0` filename" && exit $E_WRONGARGS
+[ ! -e $1 ] && echo "File \"$1\" does not exit" && exit $E_FILE_NOT_EXIST
+declare -i inum=`ls -i | grep $1 | awk '{print $1}'` && echo y/n; read
+[ $REPLY = -y ] && find . -inum $inum -exec rm {} \; : 'file was deleted' exit $?
