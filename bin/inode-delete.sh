4a987c9ff1ee5af3a477879339c9950e9cc43fe1
Ruslan <rv.volovik88@gmail.com>
2020-08-03 19:27:21 +0300

    delete via inode, perm

--- bin/inode-delete.sh
+++ bin/inode-delete.sh
@@ -8,4 +8,11 @@ readonly E_CHANGED_MIND=72
 [ $# -ne $ARGCOUNT ] && echo "Usage: `basename $0` filename" && exit $E_WRONGARGS
 [ ! -e $1 ] && echo "File \"$1\" does not exit" && exit $E_FILE_NOT_EXIST
 declare -i inum=`ls -i | grep $1 | awk '{print $1}'` && echo y/n; read
+
 [ $REPLY = -y ] && find . -inum $inum -exec rm {} \; : 'file was deleted' exit $?
+
+{
+  readonly directory=/usr/sbin
+  readonly permisions=+4000
+  for file in $(find $directory -perm $permisions); do ls -lt --clasify --author $file; done
+}
