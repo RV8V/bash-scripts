bf60e9792e0bc79528c552bf6de1ffc39d50303f
Ruslan <rv.volovik88@gmail.com>
2020-08-03 18:30:38 +0300

    web server on bash

--- bin/server.sh
+++ bin/server.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
  
 readonly RESPONSE="HTTP/1.1 200 OK\r\nConnection: keep-alive\r\n\r\n${2:-"OK"}\r\n"
-while { echo -en "$RESPONSE"; } | nc -l "${1:-8080}"; do
-  echo "web server"
-done  
+while { echo -en "$RESPONSE"; } | nc -l "${1:-8080}"; do echo "web server"; done  
