6eb7952696ed632940147357c8dfc67cc0f8f130
Ruslan <rv.volovik88@gmail.com>
2020-08-03 19:03:50 +0300

    some changes to file

--- bin/examples.sh
+++ bin/examples.sh
@@ -6,4 +6,4 @@ find . -type d -print | /usr/bin/xargs -P 4 -I {} /bin/tar czf {}.tar.gz {}
 find . -type f -and -iname '*.deb' | /usr/bin/xargs -n1 -I x dpkg -i x
 dpkg -l linux-* | awk '/^ii/{ print $2 }' | grep -e -v $(uname -r | cut -f1,2 -d"-") | grep -e [0-9]
 : 'delete files in current directory containing: + { ; " \ = ? ~ ( ) < > & * | $'
-for f in *; do readonly badname=`echo "$f" | sed -n /[\+\{\"\\\=\?~\(\)\<\>\&\*\|\$\/p` && rm $badname 2> /dev/null ;done
+for f in *; do readonly badname=`echo "$f" | sed -n /[\+\{\"\\\=\?~\(\)\<\>\&\*\|\$\/p` && rm $badname 2> /dev/null; done
