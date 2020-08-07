ea05101e63b49d0b9cd7a7229fb9aaa72709eeeb
Ruslan <rv.volovik88@gmail.com>
2020-08-07 10:09:36 +0300

    subshell example

--- bin/subshell.sh
+++ bin/subshell.sh
@@ -5,6 +5,9 @@ readonly outer=Outer
 (
   readonly inner=Inner
   echo "child process, \"inner\"=$inner"
+  (cat $1 $2 $3 | sort | uniq > list$1$2$3) &
+  (cat $4 $5 $6 | sort | uniq > list$4$5$6) & && wait
+  diff list$1$2$3 list$4$5$6 
 )
 
 [ -z $inner ] && echo "variable inner is not defined in parent process" && exit $?
