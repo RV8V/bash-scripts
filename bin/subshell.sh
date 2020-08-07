3037ec02f39c989f25b9083f2e0511692fb1de15
Ruslan <rv.volovik88@gmail.com>
2020-08-07 10:41:28 +0300

    some change to file with child process

--- bin/subshell.sh
+++ bin/subshell.sh
@@ -9,7 +9,11 @@ readonly outer=Outer
   echo "child process, \"inner\"=$inner"
   (cat $1 $2 $3 | sort | uniq > list$1$2$3) &
   (cat $4 $5 $6 | sort | uniq > list$4$5$6) & && wait
-  diff list$1$2$3 list$4$5$6 && exit $? 
+  diff list$1$2$3 list$4$5$6 && exit $?
+
+  while IFS=: read name password uid gid fullname ignore
+  do echo $name, ($fullname) and $uid; done < /etc/passwd | head
+  echo "no need to change variable \"IFS\" to previous state ->" && exit $? 
 )
 
 for home in `awk -F: '{print $6} /etc/passwd'`
