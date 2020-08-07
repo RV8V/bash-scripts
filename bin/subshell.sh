4ac1b18fc23da9e4b066bb0b2d53c429b6c8ba0b
Ruslan <rv.volovik88@gmail.com>
2020-08-07 10:18:40 +0300

    subshell example

--- bin/subshell.sh
+++ bin/subshell.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 : 'subshell.sh'
 
+readonly file=.bashrc
 readonly outer=Outer
+
 (
   readonly inner=Inner
   echo "child process, \"inner\"=$inner"
@@ -9,6 +11,13 @@ readonly outer=Outer
   (cat $4 $5 $6 | sort | uniq > list$4$5$6) & && wait
   diff list$1$2$3 list$4$5$6 
 )
+
+for home in `awk -F: '{print $6} /etc/passwd'`
+do
+   [ -d "$home" ] || continue
+   [ -r "$home" ] || continue
+   (cd $home; [ -e $file ] && less $file)
+done
 
 [ -z $inner ] && echo "variable inner is not defined in parent process" && exit $?
 echo "parent process, \"inner\"=$inner" && exit $?
